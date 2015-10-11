5a454bbed228673ad7aba084484a70cbd79a2cd3
Suz Hinton <suz.hinton@gmail.com>
2015-10-11 14:11:33 -0400

    add usbtinyisp mock helper

---
+++ tests/helpers/usbtinyisp-mock.js
@@ -0,0 +1,50 @@
+function usbtinyisp (options) {
+
+}
+
+usbtinyisp.prototype.open = function(callback) {
+  return callback(null);
+};
+
+usbtinyisp.prototype.close = function() {};
+
+usbtinyisp.prototype.spi = function (callback) {
+  return callback(null, new Buffer([0x00, 0x00, 0x00]));
+};
+
+usbtinyisp.prototype.setSCK = function (buffer, callback) {
+  return callback(null);
+};
+
+usbtinyisp.prototype.powerDown = function (callback) {
+  return callback(null);
+};
+
+usbtinyisp.prototype.readFlash = function (length, address, callback) {
+  var data = new Buffer(length);
+  data.fill(0xFF);
+  return callback(null, data);
+};
+
+usbtinyisp.prototype.readEeprom = function (length, address, callback) {
+  var data = new Buffer(length);
+  data.fill(0xFF);
+  return callback(null, data);
+};
+
+usbtinyisp.prototype.readFlash = function (length, address, callback) {
+  var data = new Buffer(length);
+  data.fill(0xFF);
+  data[1] = 0x00;
+  return callback(null, data);
+};
+
+usbtinyisp.prototype.writeFlash = function (callback) {
+  return callback(null);
+};
+
+usbtinyisp.prototype.writeEeprom = function (callback) {
+  return callback(null);
+};
+
+module.exports = usbtinyisp;
